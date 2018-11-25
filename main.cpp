e92b8755c4af62a1f8a7d3c3bfbd4684034daac4
Martin Nilsson <martin.lennart.nilsson@gmail.com>
2018-11-25 19:17:21 +0100

    Add parsing of attribute values.

--- main.cpp
+++ main.cpp
@@ -10,17 +10,32 @@ static auto current_time() {
 }
 
 int main(int, char**) {
-
     std::ifstream ifs ("../merged.vrscene", std::ifstream::in);
     std::string str((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
     std::string_view string(str);
     ifs.close();
+
     auto start = current_time();
     if(auto scene = parse_vrscene(string); scene) {
         auto vrscene = *scene;
         auto dur = current_time() - start;
 
         std::cout << "SUCCESS " << dur.count() << std::endl;
+        auto i = 0;
+        for(auto&& c : vrscene.comments) {
+            std::cout << c;
+        }
+        for(auto&& i : vrscene.includes) {
+            std::cout << i;
+        }
+
+        for(auto&& p : vrscene.plugins) {
+            if(i++ >= 10) break;
+            std::cout << p.type << " " << p.name << std::endl;
+            for(auto&& it : p.attributes) {
+                std::cout << "\t" << it.first << "=" << it.second << std::endl;
+            }
+        }
     }
     return 0;
 }
--- vrscene_parser.h
+++ vrscene_parser.h
@@ -1,20 +1,49 @@
 #pragma once
-
+#include <cstdlib>
 #include <iostream>
 #include <optional>
-#include <unordered_map>
 #include <string>
+#include <unordered_map>
+#include <variant>
 #include <vector>
 
 template<class T> using Maybe = std::optional<T>;
 
-using Include = std::string;
-using Comment = std::string;
+struct Include : public std::string {
+  using std::string::string;
+};
+
+struct Comment : public std::string {
+  using std::string::string;
+};
+
 using Identifier = std::string;
+
+struct AttributeSelector {
+    Identifier plugin;
+    Identifier attribute;
+};
+
+struct Function;
+using Value = std::variant<AttributeSelector, Function, Identifier, int64_t, double_t, std::string_view>;
+
+struct Function {
+    std::string name;
+    std::vector<Value> arguments;
+
+    Value operator[](size_t idx) const {
+        return arguments[idx];
+    }
+};
 
 struct Plugin {
     std::string name;
     std::string type;
+    std::unordered_map<Identifier, Value> attributes;
+
+    Value operator[](Identifier id) const {
+        return attributes.at(id);
+    }
 };
 
 struct Vrscene {
@@ -23,47 +52,47 @@ struct Vrscene {
     std::vector<Plugin> plugins;
 };
 
-inline static void trim_leading_whitespace(std::string_view& string) {
+inline void trim_leading_whitespace(std::string_view& string) noexcept {
     while(std::isspace(string.front())) string.remove_prefix(1);
 }
 
-inline static void trim_trailing_whitespace(std::string_view& string) {
+inline void trim_trailing_whitespace(std::string_view& string) noexcept {
     while(std::isspace(string.back())) string.remove_suffix(1);
 }
 
-inline static bool match(std::string_view string, std::string_view token) {
+inline bool match(std::string_view string, std::string_view token) noexcept {
     return string.compare(0, token.size(), token) == 0;
 }
 
-inline static bool try_consume(std::string_view& string, std::string_view token) {
+inline bool try_consume(std::string_view& string, std::string_view token, bool trim = true) noexcept {
+    if (trim) trim_leading_whitespace(string);
     if (!match(string, token)) return false;
     string.remove_prefix(token.size());
     return true;
 }
 
-inline static bool is_space(char ch){
+inline bool is_space(char ch) noexcept {
     return std::isspace(static_cast<unsigned char>(ch));
 }
 
-static Maybe<std::string_view> qouted_string(std::string_view& string) {
-    if (string[0] != '"') return {};
-    auto end = string.find('"', 1);
-    auto res = string.substr(1, end - 1);
-    string.remove_prefix(end + 1);
-    return res;
+inline Maybe<std::string_view> parse_qouted_string(std::string_view& src) {
+    if(!try_consume(src, "\"")) return {};
+    auto end = src.find('"');
+    auto val = src.substr(0, end);
+    src.remove_prefix(end + 1);
+    return val;
 }
 
-static Maybe<Include> parse_include(std::string_view& string) {
-    trim_leading_whitespace(string);
+inline Maybe<Include> parse_include(std::string_view& string) {
     if (!try_consume(string, "#include")){
         return {};
     }
     trim_leading_whitespace(string);
-    auto file = qouted_string(string);
+    auto file = parse_qouted_string(string);
     return file ? std::make_optional(Include(*file)) : std::nullopt;
 }
 
-static Maybe<Comment> parse_comment(std::string_view& source) {
+inline Maybe<Comment> parse_comment(std::string_view& source) {
     trim_leading_whitespace(source);
     if (!match(source, "//")) return {};
 
@@ -74,11 +103,14 @@ static Maybe<Comment> parse_comment(std::string_view& source) {
     return Comment(comment);
 }
 
+inline auto is_valid_identifier_start(char c) noexcept {
+    return std::isalpha(c) || static_cast<uint8_t>(c) >= 0xC0;
+}
 
-static Maybe<Identifier> parse_identifier(std::string_view& src) {
+inline Maybe<Identifier> parse_identifier(std::string_view& src) {
     trim_leading_whitespace(src);
 
-    if(!std::isalpha(src.front())) return {};
+    if(!is_valid_identifier_start(src.front())) return {};
 
     const auto is_identifier_end = [](char c) {
         static const std::string identifier_breaks = "=;{}:,()";
@@ -92,18 +124,96 @@ static Maybe<Identifier> parse_identifier(std::string_view& src) {
     return result;
 }
 
-static Maybe<Plugin> parse_plugin(std::string_view& src) {
+inline Maybe<Value> parse_number(std::string_view& src) {
+    char *ds = nullptr, *is=nullptr;
+    const auto pos = &src[0];
+    auto d = Value(std::strtod(pos, &ds));
+    auto i = Value(std::strtoll(pos, &is, 10));
+    if(ds == pos && is == pos) return {};
+    src.remove_prefix((ds > is ? ds : is) - pos);
+    return ds > is ? std::make_optional(d) : std::make_optional(i);
+}
+
+inline Maybe<Value> parse_attribute_selector(std::string_view& source) {
+    auto src = source;
+    auto p = parse_identifier(src);
+    auto dots = try_consume(src, "::", false);
+    auto a = parse_identifier(src);
+
+    if(!(p && dots && a)) return {};
+
+    source = src;
+    return AttributeSelector{*p, *a};
+}
+
+Maybe<Value> parse_function(std::string_view& src);
+
+inline Maybe<Value> parse_value(std::string_view& src) {
+    if (auto n = parse_number(src); n) {
+        return *n;
+    } else if (auto s = parse_qouted_string(src); s) {
+        return *s;
+    } else if (auto f = parse_function(src); f) {
+        return *f;
+    } else if (auto as = parse_attribute_selector(src); as) {
+        return *as;
+    }else if (auto i = parse_identifier(src); i) {
+        return *i;
+    } else {
+        return {};
+    }
+}
+
+inline Maybe<Value> parse_function(std::string_view& source) {
+    auto src = source;
+    auto n = parse_identifier(src);
+    if(!n) return {};
+    Function f;
+    f.name = *n;
+    if(!try_consume(src, "(")) return {};
+
+    // TODO: Cleanup
+    if(!try_consume(src, ")")) {
+        auto v = parse_value(src);
+
+        while(!try_consume(src, ")")) {
+            if(!try_consume(src, ",")) return {};
+            auto v = parse_value(src);
+            if(!v) return {};
+            f.arguments.push_back(*v);
+        }
+    }
+
+    source = src;
+    return f;
+}
+
+inline Maybe<Plugin> parse_plugin(std::string_view& src) {
     auto type = parse_identifier(src);
     auto name = parse_identifier(src);
     if (!(type && name)) return {};
-    auto begin = src.find('{');
-    auto end = src.find('}');
-    src.remove_prefix(end + 1);
 
-    return Plugin{*name, *type};
+    auto begin = src.find('{');
+    src.remove_prefix(begin + 1);
+    Plugin p;
+    p.name = *name;
+    p.type = *type;
+
+    while(!try_consume(src, "}")) {
+        if(auto comment = parse_comment(src); comment) continue;
+        auto id = parse_identifier(src);
+        if(!id) return {};
+        if(!try_consume(src, "=")) return {};
+        auto val = parse_value(src);
+        if(!val) return {};
+        if(!try_consume(src, ";")) return {};
+        p.attributes[*id] = *val;
+    }
+
+    return p;
 }
 
-static Maybe<Vrscene> parse_vrscene(const std::string_view& src) {
+inline Maybe<Vrscene> parse_vrscene(const std::string_view& src) {
     auto source = src;
     Vrscene scene;
 
@@ -124,4 +234,17 @@ static Maybe<Vrscene> parse_vrscene(const std::string_view& src) {
     }
 
     return scene;
+}
+
+std::ostream& operator<<(std::ostream& os, const Value& v) {
+    //std::visit([&os](auto const& e){ os << e; }, v);
+    return os << "Hej";
+}
+
+std::ostream& operator<<(std::ostream& os, const Comment& c) {
+    return os << std::string_view(c) << '\n';
+}
+
+std::ostream& operator<<(std::ostream& os, const Include& inc) {
+    return os << "#include \"" << std::string_view(inc) << "\"\n";
 }
